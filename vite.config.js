037f60f518c02947544f036b46c7f948ccbfbd72
Jarrad L <jlovelace2500k@gmail.com>
2024-04-22 01:14:41 -0700

    setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://whoremonger.github.io/random-quote-machine"
+  base: "https://whoremonger.github.io/random-quote-machine/",
 })
